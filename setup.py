98e61166207ec55bb3b6188d02df3e4931efe93c
fciamponi <felipe.ciamponi@gmail.com>
2018-11-25 15:27:55 -0200

    fix version information

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='featurefinder',
-    version='1.1.1',
+    version='1.1.3',
     packages=find_packages(),
     url='https://github.com/kbmlab/BioFeatureFinder',
     license='',
